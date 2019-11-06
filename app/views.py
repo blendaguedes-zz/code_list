fdbd0ae677a8ef22409b99d929a19bea4870af84
blenda.guedes <blenda.guedes@fastsolucoes.com.br>
2019-11-06 10:33:38 -0300

    front and back finished. final commit

--- app/views.py
+++ app/views.py
@@ -1,6 +1,48 @@
 from django.shortcuts import render
+import requests
+from requests.compat import quote_plus
+from bs4 import BeautifulSoup
+from . import models
 
+BASE_CRAIGSLIST_URL = 'https://losangeles.craigslist.org/search/?query={}'
+BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
 
 def home(request):
     return render(request, 'base.html')
 
+def new_search(request):
+    search = request.POST.get('search')
+    search = search if search != None else ''
+
+    models.Search.objects.create(search=search)
+
+    final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
+    response = requests.get(final_url)
+    data = response.text
+    soup = BeautifulSoup(data, features='html.parser')
+    posts = soup.find_all('li', {'class':'result-row'})
+
+    final_postings = []
+
+    for post in posts:
+        post_title = soup.find(class_='result-title').text
+        post_url = post.find('a').get('href')
+
+        if post.find(class_='result-price'):
+            post_price = post.find(class_='result-price').text
+        else:
+            post_price = 'N/A'
+
+        if post.find(class_='result-image').get('data-ids'):
+            post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image_url = BASE_IMAGE_URL.format(post_image_id)
+        else:
+            post_image_url = 'https://craigslist.org/images/peace.jpg'
+
+        final_postings.append((post_title, post_url, post_price, post_image_url))
+
+    to_front = {
+        'search': search,
+        'final_posting': final_postings
+    }
+    return render(request, 'app/new_search.html', to_front)
